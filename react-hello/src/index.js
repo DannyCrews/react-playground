a33ad47d0dd9d9de323f85357386076dc88608fb
dannycrews <dancrews@me.com>
2016-08-23 23:46:03 -0400

    A fancier Hello, World

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -1,1 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Hello/> <World/>!
+      </div>
+      // React.createElement('div', {}, 'Hello, World!')
+    );
+  }
+});
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <span>Hello</span>
+    );
+  }
+});
+
+var World = React.createClass({
+  render: function() {
+    return (
+      <span>World</span>
+    );
+  }
+});
+
+ReactDOM.render(<HelloWorld/>, document.querySelector('#root'));
