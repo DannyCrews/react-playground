5fa64e85031aca74d7b528de579c3bb8ba4d5ce9
dannycrews <dancrews@me.com>
2016-08-24 07:55:45 -0400

    Save exercises

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -5,7 +5,7 @@ var ComponentName = React.createClass({
   render: function() {
     return (
       <div className="book">
-        <div className="title">The Title</div>
+        <div className="title">{'"Quoted string"'}</div>
         <div className="author">The Author</div>
         <ul className="stats">
           <li className="rating">5 stars</li>
@@ -15,8 +15,41 @@ var ComponentName = React.createClass({
     );
   }
 });
+//
+// var ComponentName = React.createClass({
+//   render: function() {
+//     return (
+//       React.createElement('div', {className: 'book'},
+//         React.createElement('div', {className: 'title'}, 'The Title'),
+//         React.createElement('div', {className: 'author'}, 'The Author'),
+//         React.createElement('ul', {className: 'stats'},
+//           React.createElement('li', {className: 'rating'}, '5 stars'),
+//           React.createElement('li', {className: 'isbn'}, '12-345678-910')
+//         )
+//       )
+//     );
+//   }
+// });
 
 ReactDOM.render(
   <ComponentName/>,
   document.getElementById('root')
 );
+
+// var LoginIndicator = React.createClass({
+//   render: function() {
+//     // var username = "root";
+//     // var username = undefined;
+//     // var username = null;
+//     var username = false;
+
+//     return(
+//       <div>{username ? `Hello, ${username}` : 'Not logged in'}</div>
+//     );
+//   }
+// });
+
+// ReactDOM.render(
+//   <LoginIndicator/>,
+//   document.getElementById('root')
+// );
