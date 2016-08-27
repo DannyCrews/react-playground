8e9568575555f2e90d426bbf1db4e3229d5d6572
dannycrews <dancrews@me.com>
2016-08-27 18:23:31 -0400

    Finish up component and add styling

--- props-exercises/email-component/src/index.js
+++ props-exercises/email-component/src/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+
+
+var MailItem = React.createClass({
+  propTypes: {
+    email: React.PropTypes.shape({
+      sender: React.PropTypes.string.isRequired,
+      subject: React.PropTypes.string.isRequired,
+      date: React.PropTypes.string.isRequired,
+      message: React.PropTypes.string.isRequired
+    }).isRequired
+  },
+  render: function() {
+    var {sender, subject, date, message} = this.props.email;
+    return (
+      <div className="email">
+        <input type="checkbox"/>
+        <i className="fa fa-thumb-tack pin"/>
+        <div className="content">
+          <div className="line1">
+            <div className="sender">{sender}</div>
+            <div className="subject">{subject}</div>
+            <div className="date">{date}</div>
+          </div>
+          <div className="message">{message}</div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var emailData = {
+  sender: "React NewsLetter",
+  subject: "React NewsLetter - Issue 36",
+  date: "Jul 15, 2014",
+  message: "This is the message"
+};
+
 ReactDOM.render(
-  <App />,
+  <MailItem email={emailData} />,
   document.getElementById('root')
 );
