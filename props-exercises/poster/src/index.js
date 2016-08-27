de3c9e86d3d9529203c14c38bf9a69db30cfd19f
dannycrews <dancrews@me.com>
2016-08-27 14:04:52 -0400

    create Poster component

--- props-exercises/poster/src/index.js
+++ props-exercises/poster/src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import image from './logo.svg'
+
+var posterImage = image;
+var posterTitle = 'ReacT';
+var posterText = 'The best thing since jQuery, probably.'
+
+
+
+var Poster = React.createClass ({
+  render: function() {
+    return (
+      <div className='poster'>
+        <div className='image'>
+          <img src={this.props.image} alt='react' />
+        </div>
+        <div className='title'>
+          {this.props.title}
+        </div>
+        <div className='text'>
+          {this.props.text}
+        </div>
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
-  <App />,
+  <Poster image={posterImage} title={posterTitle} text={posterText}/>,
   document.getElementById('root')
 );
